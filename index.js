67846f8631e9fbc20a5e5e82a347f7711d91472e
willmondal <wbmondal@aol.com>
2021-02-11 10:55:09 -0800

    monolith again

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const dotenv = require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
+const path = require('path')
 
 const app = express()
 const port = process.env.PORT || 9000
 
 app.use(cors())
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "client/build", "index.html")))
+
 
 app.use('/api/*', ( _ , res) => { 
     res.json({data: "The API lives here"})
